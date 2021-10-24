e58928d4b736c37cb3e279193aa7b3f2119598b7
TriPham <phamductri2001@gmail.com>
2021-10-24 15:07:50 +0700

    update 24/10/2021

--- assets/js/components/category-js/Banner.js
+++ assets/js/components/category-js/Banner.js
@@ -1,21 +1,42 @@
 function Banner() {
     const slide = document.querySelectorAll(".new-book-slider-wrapper__slider__slides__slide");
-    const invisibleBlock = document.querySelector(".new-book-slider-wrapper__slider__slides");
+    const slides = document.querySelector(".new-book-slider-wrapper__slider__slides");
+
     let count = 0;
-    let length = 3;
+    let length = 0;
+    if(document.body.clientWidth >= 1024)
+        length = 4;
+    else if (document.body.clientWidth >= 840 && document.body.clientWidth <= 1023)
+        length = 3;
+    else if (document.body.clientWidth <= 839)
+        length = 2;
+    console.log(length);
+    window.addEventListener("resize", () => {
+        slides.style.marginLeft = "0px";
+        count = 0;
+        if(document.body.clientWidth >= 1024)
+            length = 4;
+        else if (document.body.clientWidth >= 840 && document.body.clientWidth <= 1023)
+            length = 3;
+        else if (document.body.clientWidth <= 839)
+            length = 2;
+    })
+
     document.querySelector("#button-arrow-left").addEventListener("click", () => {
-        if(length >= slide.length)
+        if(length >= slide.length + 1)
             return;
         count++;
         length++;
-        invisibleBlock.style.marginLeft = `${-25 * count}%`;
+        slides.style.marginLeft = `${-(slide[0].clientWidth + 9 * 2) * count}px`;
+        console.log(length);
     })
     document.querySelector("#button-arrow-right").addEventListener("click", () => {
-        if(length <= 3)
+        if(count <= 0)
             return;
         count--;
         length--;
-        invisibleBlock.style.marginLeft = `${-25 * count}%`;
+        slides.style.marginLeft = `${-(slide[0].clientWidth + 9 * 2) * count}px`;
+        console.log(length);
     })
 }
 export {Banner};
